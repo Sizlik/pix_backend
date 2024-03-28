9622d2a5c3168c5e1e43469c44f83718947722c9
Sizlik <43879572+Sizlik@users.noreply.github.com>
2024-03-28 07:47:40 +0200

    some fixes

--- db/schemas/users.py
+++ db/schemas/users.py
@@ -11,6 +11,7 @@ class BaseUser(BaseModel):
     phone_number: str
     is_organization_user: bool = False
     organization_id: Optional[uuid.UUID] = None
+    is_organization_user: Optional[bool]
 
 
 class UserRead(schemas.BaseUser[uuid.UUID], BaseUser):
@@ -22,6 +23,9 @@ class UserCreate(schemas.BaseUserCreate, BaseUser):
 
 
 class UserUpdate(schemas.BaseUserUpdate):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    phone_number: Optional[str] = None
     moysklad_counterparty_id: Optional[uuid.UUID] = None
     moysklad_counterparty_meta: Optional[dict] = None
     balance: Optional[int] = None
